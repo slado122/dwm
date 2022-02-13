e25d2bf56bf3fc6783e717796ddfe31a16b7b434
Dmytro Ohorodnik <me@dmytroohorodnik.com>
2022-02-13 13:08:09 +0100

    Apply updates from git.suckless.org

--- shiftview.c
+++ shiftview.c
@@ -3,66 +3,62 @@
  * @param: "arg->i" stores the number of tags to shift right (positive value)
  *          or left (negative value)
  */
-void
-shiftview(const Arg *arg)
-{
-	Arg a;
-	Client *c;
-	unsigned visible = 0;
-	int i = arg->i;
-	int count = 0;
-	int nextseltags, curseltags = selmon->tagset[selmon->seltags];
+void shiftview(const Arg *arg) {
+  Arg a;
+  Client *c;
+  unsigned visible = 0;
+  int i = arg->i;
+  int count = 0;
+  int nextseltags, curseltags = selmon->tagset[selmon->seltags];
 
-	do {
-		if(i > 0) // left circular shift
-			nextseltags = (curseltags << i) | (curseltags >> (LENGTH(tags) - i));
+  do {
+    if (i > 0) // left circular shift
+      nextseltags = (curseltags << i) | (curseltags >> (LENGTH(tags) - i));
 
-		else // right circular shift
-			nextseltags = curseltags >> (- i) | (curseltags << (LENGTH(tags) + i));
+    else // right circular shift
+      nextseltags = curseltags >> (-i) | (curseltags << (LENGTH(tags) + i));
 
-                // Check if tag is visible
-		for (c = selmon->clients; c && !visible; c = c->next)
-			if (nextseltags & c->tags) {
-				visible = 1;
-				break;
-			}
-		i += arg->i;
-	} while (!visible && ++count < 10);
+    // Check if tag is visible
+    for (c = selmon->clients; c && !visible; c = c->next)
+      if (nextseltags & c->tags) {
+        visible = 1;
+        break;
+      }
+    i += arg->i;
+  } while (!visible && ++count < 10);
 
-	if (count < 10) {
-		a.i = nextseltags;
-		view(&a);
-	}
+  if (count < 10) {
+    a.i = nextseltags;
+    view(&a);
+  }
 }
 
-void
-shifttag(const Arg *arg)
-{
-	Arg a;
-	Client *c;
-	unsigned visible = 0;
-	int i = arg->i;
-	int count = 0;
-	int nextseltags, curseltags = selmon->tagset[selmon->seltags];
+void shifttag(const Arg *arg) {
+  Arg a;
+  Client *c;
+  unsigned visible = 0;
+  int i = arg->i;
+  int count = 0;
+  int nextseltags, curseltags = selmon->tagset[selmon->seltags];
 
-	do {
-		if(i > 0) // left circular shift
-			nextseltags = (curseltags << i) | (curseltags >> (LENGTH(tags) - i));
+  do {
+    if (i > 0) // left circular shift
+      nextseltags = (curseltags << i) | (curseltags >> (LENGTH(tags) - i));
 
-		else // right circular shift
-			nextseltags = curseltags >> (- i) | (curseltags << (LENGTH(tags) + i));
+    else // right circular shift
+      nextseltags = curseltags >> (-i) | (curseltags << (LENGTH(tags) + i));
 
-                // Check if tag is visible
-		for (c = selmon->clients; c && !visible; c = c->next)
-			if (nextseltags & c->tags) {
-				visible = 1;
-				break;
-			}
-		i += arg->i;
-	} while (!visible && ++count < 10);
+    // Check if tag is visible
+    for (c = selmon->clients; c && !visible; c = c->next)
+      if (nextseltags & c->tags) {
+        visible = 1;
+        break;
+      }
+    i += arg->i;
+  } while (!visible && ++count < 10);
 
-	if (count < 10) {
-		a.i = nextseltags;
-		tag(&a);
-	}
+  if (count < 10) {
+    a.i = nextseltags;
+    tag(&a);
+  }
 }
